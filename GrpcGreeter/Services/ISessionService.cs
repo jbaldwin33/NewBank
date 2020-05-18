3184e0dd9a7e101173005a451d2ff8cf0d2216d4
jbaldwin33 <jbaldwin93@gatech.edu>
2020-05-18 18:14:23 -0400

    added session service

---
+++ GrpcGreeter/Services/ISessionService.cs
@@ -0,0 +1,13 @@
+﻿using GrpcGreeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrpcGreeter.Services
+{
+  public interface ISessionService
+  {
+    SessionProperties SessionProperties { get; }
+    void UpdateCurrentUser(UserModel user);
+  }
+}
---
+++ GrpcGreeter/Services/SessionService.cs
@@ -0,0 +1,34 @@
+﻿using GrpcGreeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrpcGreeter.Services
+{
+  public class SessionService : ISessionService
+  {
+    private SessionProperties sessionProperties;
+    public SessionProperties SessionProperties
+    {
+      get => sessionProperties;
+      set => sessionProperties = value;
+    }
+
+    public void UpdateCurrentUser(UserModel user)
+    {
+      SessionProperties.CurrentUser = user;
+    }
+
+    public SessionService()
+    {
+      SessionProperties = new SessionProperties { SessionID = Guid.NewGuid() };
+    }
+  }
+
+  public class SessionProperties
+  {
+    public Guid SessionID { get; internal set; }
+
+    public UserModel CurrentUser { get; internal set; }
+  }
+}
